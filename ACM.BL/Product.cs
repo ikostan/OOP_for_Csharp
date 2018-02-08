16e88e2954a9fe7472258f9fe0f62ca8e6569e7c
ikostan <igorkostan@gmail.com>
2018-02-07 18:06:28 -0800

    Product class refactoring

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -8,5 +8,52 @@ namespace ACM.BL
 {
     class Product
     {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            this.ProductId = productId;
+        }
+
+        public Decimal? CurrentPrice { get; set; }
+        public int ProductId { get; private set; }
+        public string ProductDescription { get; set; }
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Retrieve one product.
+        /// </summary>
+        public Product Retrieve(int productId)
+        {
+            // Code that retrieves the defined product
+            return new Product();
+        }
+
+        /// <summary>
+        /// Saves the current product.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined product
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the product data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
     }
 }

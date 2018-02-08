5042a4371dbe7ee672b5ddf9451c11296f719af9
ikostan <igorkostan@gmail.com>
2018-02-07 18:04:49 -0800

    Order class

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
+    /// <summary>
+    /// Holds information about an order
+    /// </summary>
     class Order
     {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            this.OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        /// <summary>
+        /// Retrieve one order.
+        /// </summary>
+        public Order Retrieve(int orderId)
+        {
+            // Code that retrieves the defined order
+            return new Order();
+        }
+
+        /// <summary>
+        /// Saves the current order.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined order
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the order data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
     }
 }
